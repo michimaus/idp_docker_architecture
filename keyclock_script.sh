5201b54b99d131553e8b30a65dc2e94a2191f316
Mihnea Duican <mduican@adobe.com>
2022-05-08 23:47:21 +0300

    GItignore and instance for keyclock and api server

---
+++ keyclock_script.sh
@@ -0,0 +1,12 @@
+#/bin/zsh
+
+VERSION=16.1.1 # set version here
+
+cd /tmp
+git clone git@github.com:keycloak/keycloak-containers.git
+cd keycloak-containers/server
+git checkout $VERSION
+docker build -t "jboss/keycloak:${VERSION}" .
+docker build -t "quay.io/keycloak/keycloak:${VERSION}" .
+
+
